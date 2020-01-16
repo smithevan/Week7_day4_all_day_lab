4b6dd38f1576e64426a5cbf7db1952d7d45d112c
Evan Smith <smith.m.evan@gmail.com>
2020-01-16 15:16:29 +0000

    now getting whole object back

---
+++ src/components/FavouriteBeers.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div v-if="beer" id="beerDetail">
+    <h2>{{beer.name}}</h2>
+    <p><span></span></p>
+    <p></p>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/components/SelectedBeers.vue
+++ src/components/SelectedBeers.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <div>
     <!-- <form v-on:submit.prevent> -->
-      <select v-on:change="handleSelect" v-model="selectedBeer">
-        <option v-for="(beer, index) in beers" :beer="beer" key="index">{{beer.name}}</option>
+      <select v-on:change="handleSelect" v-model="selectedBeer"> <!--v-model selectedBeer sends selected beer to event bus emitter-->
+        <option v-for="(beer, index) in beers" :value="beer" key="index">{{beer.name}}</option>
         <!--looping over beer in beers with index, binding individual beer(banana/iterator) onto "beer",
         which gets used in the eventbus, and index is used as the hash key-->
       </select>
@@ -19,7 +19,7 @@ export default {
   props: ['beers'],
   methods: {
     handleSelect(){
-      eventBus.$emit('beer-selected', this.selectedBeer) //whatever is asked for in v-model is needed in event bus 
+      eventBus.$emit('beer-selected', this.selectedBeer) //whatever is asked for in v-model is needed in event bus
     }
   }
 }
